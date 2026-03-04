42381efe65706b40794f62eab2c6b24a6c94586c
Zeeshan Ayyub <66298499+linkzeeshan@users.noreply.github.com>
2026-03-04 14:40:59 +0800

    Initial create MVC project
    Initial create MVC project

---
+++ MkTechSys.AgentRegistrationForm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MkTechSys.AgentRegistrationForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
